3524fe7db2e47c9156ce2f108fc2604424e570ba
Mal3uz <phuongnampham7823@gmail.com>
2024-03-21 23:52:12 +0700

    solution9

--- src/java/solution9/Solution9.java
+++ src/java/solution9/Solution9.java
@@ -2,13 +2,13 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package number.solution9;
+package solution9;
 
 /**
  *
  * @author DELL
  */
-public class Solution {
+public class Solution9 {
      public static boolean isPalindrome(int x) {
           if(x < 0){
          return false;
